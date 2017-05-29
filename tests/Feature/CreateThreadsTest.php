e510e666936271c03f0dd63b97ec39832cfd38a8
Michael Hopkins <mhopkins321@gmail.com>
2017-05-29 06:20:15 -0600

    lesson 10

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,31 +2,60 @@
 
 namespace Tests\Feature;
 
+use function create;
+use Forum\Channel;
 use Forum\Thread;
 use Illuminate\Auth\AuthenticationException;
+use function make;
 use Tests\TestCase;
 
 class CreateThreadsTest extends TestCase
 {
+	/** @test */
+	function guests_may_not_create_threads() {
+		$this->withExceptionHandling();
+		$this->post( 'threads')
+		     ->assertRedirect('/login');
+		$this->get( 'threads/create')
+		     ->assertRedirect('/login');
+	}
 
 	/** @test */
 	function an_authenticated_user_can_create_forum_threads()
 	{
 		$this->signIn();
-		$thread = create( Thread::class );
-		$this->post( '/threads',$thread->toArray());
-		$this->get( $thread->path())
+		$thread = make( Thread::class );
+		$response = $this->post( '/threads',$thread->toArray());
+		$this->get($response->headers->get('Location'))
 		     ->assertSee( $thread->title )
 		     ->assertSee( $thread->body );
 	}
 
 	/** @test */
-	function guests_may_not_create_threads() {
-		$this->withExceptionHandling();
+	function a_thread_requires_a_title()
+	{
+		$this->publishThread(['title' => null])
+			->assertSessionHasErrors('title');
+	}
+	/** @test */
+	function a_thread_requires_a_body()
+	{
+		$this->publishThread(['body' => null])
+		     ->assertSessionHasErrors('body');
+	}
+	/** @test */
+	function a_thread_requires_a_valid_channel()
+	{
+		factory(Channel::class,2)->create();
+		$this->publishThread(['channel_id' => null])
+		     ->assertSessionHasErrors('channel_id');
+		$this->publishThread(['channel_id' => 999])
+		     ->assertSessionHasErrors('channel_id');
+	}
 
-		$this->post( 'threads')
-		     ->assertRedirect('/login');
-		$this->get( 'threads/create')
-		     ->assertRedirect('/login');
+	private function publishThread( $array ) {
+		$this->withExceptionHandling()->signIn();
+		$thread = make( Thread::class,$array);
+		return $this->post( '/threads',$thread->toArray());
 	}
 }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,6 +10,14 @@ use Tests\TestCase;
 
 class ParticipateInForumTest extends TestCase
 {
+	/** @test */
+	function unauthenticated_users_may_not_participate_in_forum_threads()
+	{
+		$this->withExceptionHandling()
+		     ->post('threads/some-channel/1/replies',[])
+		     ->assertRedirect('/login');
+	}
+
     /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
     {
@@ -21,13 +29,15 @@ class ParticipateInForumTest extends TestCase
 		    ->assertSee( $reply->body);
     }
 
-	/** @test */
-	function unauthenticated_users_may_not_participate_in_forum_threads()
-	{
-		$this->withExceptionHandling()
-		     ->post('threads/some-channel/1/replies',[])
-		     ->assertRedirect('/login');
-	}
+    /** @test */
+    function a_reply_requires_a_body()
+    {
+    	$this->withExceptionHandling()->signIn();
+	    $thread = create( Thread::class );
+	    $reply  = make( Reply::class ,['body' => null]);
+    	$this->post( $thread->path().'/replies',$reply->toArray())
+		    ->assertSessionHasErrors('body');
+    }
 
 
 }
