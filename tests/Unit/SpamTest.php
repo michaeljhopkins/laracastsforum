a7f337e6b953d7631ccd819699fcfa001258d5a7
Michael Hopkins <mhopkins321@gmail.com>
2017-09-19 18:36:47 -0500

    lesson 50

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -3,14 +3,24 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use App\Spam;
+use App\Inspections\Spam;
 
 class SpamTest extends TestCase
 {
     /** @test */
-    function it_validates_spam()
+    function it_checks_for_invalid_keywords()
     {
         $spam = new Spam();
         $this->assertFalse($spam->detect('innocent'));
+        $this->expectException('exception');
+        $spam->detect('yahoo customer support');
+    }
+
+    /** @test */
+    function it_checks_for_any_key_being_held_down()
+    {
+        $spam = new Spam();
+        $this->expectException('exception');
+        $spam->detect('hello world aaaaaaaaa');
     }
 }
