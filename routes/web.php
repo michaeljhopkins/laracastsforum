099a4835736656b98a5273f876cf1eb6ba251653
Michael Hopkins <mhopkins321@gmail.com>
2017-09-27 01:05:40 -0500

    lesson 63

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
 Route::get('/profiles/{user}/notifications', 'UserNotificationsController@index');
 Route::delete('/profiles/{user}/notifications/{notification}', 'UserNotificationsController@destroy');
 Route::get('api/users', 'Api\UsersController@index');
+Route::post('api/users/{user}/avatar', 'Api\UserAvatarsController@store')->middleware('auth');
---
+++ tests/Feature/AddAvatarTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+use Storage;
+
+class AddAvatarTest extends TestCase
+{
+    use DatabaseMigrations;
+    /** @test */
+    function only_members_can_add_avatars()
+    {
+        $this->withExceptionHandling();
+        $this->json('POST','api/users/1/avatar')
+            ->assertStatus(401);
+    }
+
+    /** @test */
+    function a_valid_avatar_must_be_provided()
+    {
+        $this->withExceptionHandling()->signIn();
+        $this->json('POST','api/users/'.auth()->id().'/avatar', [
+            'avatar' => 'not-an-image'
+        ])->assertStatus(422);
+    }
+
+    /** @test */
+    function a_user_may_add_an_avatar_to_their_profile()
+    {
+        $this->signIn();
+        Storage::fake('public');
+        $file = UploadedFile::fake()->image('avatar.jpg');
+        $this->json('POST','api/users/'.auth()->id().'/avatar', [
+            'avatar' => $file
+        ]);
+        $this->assertEquals('avatars/'.$file->hashName(),auth()->user()->fresh()->avatar_path);
+        Storage::disk('public')->assertExists('avatars/'.$file->hashName());
+    }
+}
