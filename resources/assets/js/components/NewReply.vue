ee861ed0a11cb38bc2c16cc62e63f98030d997c7
Michael Hopkins <mhopkins321@gmail.com>
2017-10-04 03:32:00 -0500

    lesson 80

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -32,11 +32,6 @@
                 body: ''
             };
         },
-        computed: {
-            signedIn() {
-                return window.App.signedIn;
-            }
-        },
         mounted() {
             $('#body').atwho({
                 at: '@',
